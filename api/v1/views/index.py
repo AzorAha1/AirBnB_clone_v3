79fc7e7f008a1a6dda1bcdb8b5e4e158d6394750
AzorAha1 <wizfaiz@icloud.com>
2024-02-04 02:05:22 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -11,16 +18,9 @@ def status():
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/stats')
 def stats():
     """Give the stats in a Json format"""
-    from models import storage
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
     return jsonify({
         'amenities': storage.count(Amenity),
         'cities': storage.count(City),
