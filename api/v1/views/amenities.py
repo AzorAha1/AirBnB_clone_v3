7291b5f8d20ac5aeb75be6ccdba7429be42715b4
AzorAha1 <wizfaiz@icloud.com>
2024-02-05 07:54:05 +0100

    update

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -49,7 +49,7 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('amenities/<amenity_id>')
+@app_views.route('amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """update amenity"""
     amenity = storage.get(Amenity, amenity_id)
