0e1caafaf97bed33f176ca6b17b2a1984a358bd1
AzorAha1 <wizfaiz@icloud.com>
2024-02-05 01:30:09 +0100

    updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -62,5 +62,5 @@ def update_state(state_id):
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
-    state.save()
+    storage.save()
     return jsonify(state.to_dict()), 200
