913b52fe646c19efb93373b94aa06f8876df081b
AzorAha1 <wizfaiz@icloud.com>
2024-02-06 03:53:12 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
 
 
